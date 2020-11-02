1f88e5d0d741643c35a113757dde8f1351cecdd3
Storm <luca.schifano005@gmail.com>
2020-11-02 13:45:16 +0100

    Mise en place du perso (mouv tire) et décors

---
+++ rookieJamCloud/Assets/Luca/Script/PlayerMove.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;//Equipe Nuage
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speedH;
+    public float speedV;
+    private float speedHBase;
+    private float speedVBase;
+
+    public Transform firePoint;
+    public GameObject bullet;
+
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rb.velocity = new Vector2(speedHBase, speedVBase); //Movement
+
+           if(Input.GetKey("z")){
+               speedVBase = speedV;
+           }else if(Input.GetKey("s")){
+               speedVBase = -speedV;
+           }else{
+               speedVBase = 0;
+           }
+
+           if(Input.GetKey("q")){
+               speedHBase = -speedH;
+           }else if(Input.GetKey("d")){
+               speedHBase = speedH;
+           }else{
+               speedHBase = 0;
+           }
+
+
+           if(Input.GetMouseButtonDown(0)){                
+              if (timeBtwAttack <=0){
+               Instantiate(bullet, firePoint.position, firePoint.rotation);
+               timeBtwAttack = startTimeBtwAttack;
+               }
+           } else {
+               timeBtwAttack -= Time.deltaTime;
+           }
+
+    }
+}
