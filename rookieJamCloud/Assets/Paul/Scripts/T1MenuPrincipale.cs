e0ee06e92809507b01f137a49d0e2a5d9e84899a
Paul <paulp78@live.fr>
2020-11-03 10:29:20 +0100

    Modif du menu

--- rookieJamCloud/Assets/Paul/Scripts/T1MenuPrincipale.cs
+++ rookieJamCloud/Assets/Paul/Scripts/T1MenuPrincipale.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Boutons : MonoBehaviour {
+public class T1MenuPrincipale : MonoBehaviour {
     public void lancerJeu()
     {
         SceneManager.LoadScene("T1GamePaul", LoadSceneMode.Single);
+    }
+
+    public void quitterJeu()
+    {
+        Debug.Log("QUITTER");
+        Application.Quit();
     }
 
 }
