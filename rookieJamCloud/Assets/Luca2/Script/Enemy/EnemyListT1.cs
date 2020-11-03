1e6066c41d06cbb43df1725348e73a6fe5a48de5
Storm <luca.schifano005@gmail.com>
2020-11-03 10:18:24 +0100

    G tout refait sauf les anim

---
+++ rookieJamCloud/Assets/Luca2/Script/Enemy/EnemyListT1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyListT1 : MonoBehaviour
+{
+    public int numberOfEnnemies;
+    public bool OneTime;
+    void Start()
+    {
+        foreach (var enemy in GameObject.FindGameObjectsWithTag("Enemy"))
+         {
+             numberOfEnnemies += 1;
+         }
+    }
+
+    void Update()
+    {
+        if(numberOfEnnemies == 0 && OneTime == false){
+            StartCoroutine("EndOfBattle");
+        }
+    }
+
+    IEnumerator EndOfBattle(){
+        OneTime = true;
+        yield return new WaitForSeconds(0.2f);
+        Debug.Log("GG t un bg");
+     }
+}
---
+++ rookieJamCloud/Assets/Luca2/Script/Enemy/HpEnemyT1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpEnemyT1 : MonoBehaviour
+{
+    public int maxHp;
+    public int currentHP;
+
+    public bool mother;
+    public GameObject son;
+
+    void Start()
+    {
+        currentHP = maxHp;
+    }
+
+    void Update()
+    {
+        if(currentHP <= 0){
+            GameObject.Find("EnemyList").GetComponent<EnemyListT1>().numberOfEnnemies -= 1;
+
+            if(mother){
+                son.GetComponent<FollowT1>().enabled = false;
+                son.GetComponent<EnemyMoveT1>().enabled = true;
+            }
+            
+            Destroy(gameObject);
+        }
+    }
+
+    public void takeDamage(int damage){
+        currentHP -= damage;
+    }
+}
