74b3628f915de3de4fcf8f2670ae00a8ad9c3a96
Paul <paulp78@live.fr>
2020-11-04 17:58:23 +0100

    Ajout Roues et modifs menu

--- rookieJamCloud/Assets/Paul/Scripts/T1ItemUiHandle.cs
+++ rookieJamCloud/Assets/Paul/Scripts/T1ItemUiHandle.cs
@@ -1,47 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
-public class arrayDisplay : MonoBehaviour
+public class T1ItemUiHandle : MonoBehaviour
 {
+    public List<Sprite> Armes = new List<Sprite>();
+    public Image ImageWeapon;
 
-    public int[] displayHolder;
-    public int[] tempArray;
-    public int counter;
-
-    void Start()
+    public void getRandomweapon()
     {
-
-        for (int i = 0; i < 3; i++)
-        {
-            tempArray[i] = displayHolder[i];     
-        }
-        StartCoroutine(arrayDisplayer());
-
-    }
-
-    IEnumerator arrayDisplayer()
-    {
-        Debug.Log("tempArray" + tempArray[0]);
-        Debug.Log("tempArray" + tempArray[1]);
-        Debug.Log("tempArray" + tempArray[2]);
-        yield return new WaitForSeconds(1);
-
-        if (counter >= 9)
-            counter = 0;
-        else
-            counter++;
-        tempArray[0] = displayHolder[counter];    
-        if (counter >= 9)
-            counter = 0;
-        else
-            counter++;
-        tempArray[1] = displayHolder[counter];    
-        if (counter >= 9)
-            counter = 0;
-        else
-            counter++;
-        tempArray[2] = displayHolder[counter];   
-
-        StartCoroutine(arrayDisplayer());
+        int id = Random.Range(0, Armes.Count);
+        ImageWeapon.sprite = Armes[id];
     }
 }
