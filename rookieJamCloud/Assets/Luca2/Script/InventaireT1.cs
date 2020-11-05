88e39234563577f89706c92876704d2d785d2c21
Storm <luca.schifano005@gmail.com>
2020-11-05 10:16:36 +0100

    New Sprites + Roulette Work

--- rookieJamCloud/Assets/Luca2/Script/InventaireT1.cs
+++ rookieJamCloud/Assets/Luca2/Script/InventaireT1.cs
@@ -7,12 +7,36 @@ public class InventaireT1 : MonoBehaviour
 {
     public List<ItemT1> Deck = new List<ItemT1>(4); // Dans l'inventaire
 
-    public List<ItemT1> AllItem = new List<ItemT1>(); // Tous les item dispo
+    public List<ItemT1> AllItem = new List<ItemT1>(); // Tous les item dispo pour la game
+
+    public List<ItemT1> AllItemTrue = new List<ItemT1>(); // Tous les item dispo en tt
 
     public int maxItem;
 
     void Start()
     {
+        var logosys = GameObject.Find("LogoSystem").GetComponent<LogoSystem>();
+        if(logosys.logo1 == 0 || logosys.logo2 == 0){
+            AllItem.Add(AllItemTrue[0]);
+        }
+        if(logosys.logo1 == 1 || logosys.logo2 == 1){
+            AllItem.Add(AllItemTrue[1]);
+        }
+        if(logosys.logo1 == 2 || logosys.logo2 == 2){
+            AllItem.Add(AllItemTrue[2]);
+        }
+        if(logosys.logo1 == 3 || logosys.logo2 == 3){
+            AllItem.Add(AllItemTrue[3]);
+        }
+        if(logosys.logo1 == 4 || logosys.logo2 == 4){
+            AllItem.Add(AllItemTrue[4]);
+        }
+        if(logosys.logo1 == 5 || logosys.logo2 == 5){
+            AllItem.Add(AllItemTrue[5]);
+        }
+        if(logosys.logo1 == 6 || logosys.logo2 == 6){
+            AllItem.Add(AllItemTrue[6]);
+        }
         majItem();
     }
 
